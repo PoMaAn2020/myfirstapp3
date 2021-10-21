c8ee27b4b223ff95906087ef6fcf37187788b7c6
PoAnMa2020 <poanma@yandex.ru>
2021-10-21 21:18:22 +0400

    first commit

---
+++ app/src/main/java/com/pomaan/myfirstapp3/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.pomaan.myfirstapp3
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
